32048d41b09a47231a8f25febed0ad03947e153d
Btari Fatma Rosa <btari.f.r@mail.ugm.ac.id>
2022-11-29 11:41:20 +0700

    Add classes

---
+++ ResponsiJunProBFR/Departemen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiJunProBFR
+{
+    internal class Departemen
+    {
+        private int _id_dep;
+        private string _nama_dep;
+
+        public int Id_dep { get => _id_dep; set => _id_dep = value; }
+        public string Nama_dep { get => _nama_dep; set => _nama_dep = value; }
+    }
+}
---
+++ ResponsiJunProBFR/Karyawan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiJunProBFR
+{
+    internal class Karyawan: Departemen
+    {
+        private string _id_karyawan;
+        private string _nama;
+        private int _id_dep;
+
+        public string Id_karyawan { get => _id_karyawan; set => _id_karyawan = value; }
+        public string Nama { get => _nama; set => _nama = value; }
+    }
+}
